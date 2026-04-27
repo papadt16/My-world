62d4becd04dd3fa6b8bcb4a7d634a50b94deef47
papadt16 <149633903+papadt16@users.noreply.github.com>
2026-04-27 11:33:20 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,11 +6,18 @@ window.GLOBE_GALLERY_CONFIG = {
       apiKey: "AIzaSyDxArmYBl-RnWUlMzBpSyc-OPfd0SsqnHE",
       authDomain: "orbfolio-a987b.firebaseapp.com",
       projectId: "orbfolio-a987b",
-      storageBucket: "orbfolio-a987b.firebasestorage.app",
       messagingSenderId: "442129089962",
       appId: "1:442129089962:web:3aa5638f064ee61b0157d3"
     }
   },
+
+   cloudinary: {
+    enabled: true,
+    cloudName: "dj9eu4sqm",
+    uploadPreset: "orbfolio_unsigned",
+    folder: "orbfolio"
+  },
+  
   gallery: {
     maxImages: 96,
     maxUploadSizeMb: 12
