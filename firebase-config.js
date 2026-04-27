d6342ecb897b389c276addf0d83ac1ffcc395f04
papadt16 <149633903+papadt16@users.noreply.github.com>
2026-04-27 10:29:02 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.GLOBE_GALLERY_CONFIG = {
+  appName: "Orbfolio",
+  firebase: {
+    enabled: false,
+    config: {
+      apiKey: "REPLACE_ME",
+      authDomain: "REPLACE_ME.firebaseapp.com",
+      projectId: "REPLACE_ME",
+      storageBucket: "REPLACE_ME.appspot.com",
+      messagingSenderId: "REPLACE_ME",
+      appId: "REPLACE_ME"
+    }
+  },
+  gallery: {
+    maxImages: 96,
+    maxUploadSizeMb: 12
+  }
+};
